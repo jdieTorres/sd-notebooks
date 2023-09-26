8dd03de5de2cc9fed6d11555a191cf7ed2f878cc
Juan Aguirre <juandie.torres04@gmail.com>
2023-09-25 23:46:46 -0500

    Notes view Created

---
+++ php/Connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$conexion = mysqli_connect("localhost", "root", "JadT@0000", "Register_User_Login");
+
+if ($conexion) {
+    
+}else{
+    echo 'La base de datos no fue conectada';
+}
+?>
